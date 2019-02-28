90f9015e4e17d280bcc3d326d049721749750963
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-28 09:07:29 -0500

    add new and delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,12 +16,33 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     @project.update(project_params)
     if @project.save
-      flash[:notice] = "Project updated successfully"
+      flash[:notice] = "Project updated successfully!"
       redirect_to @project
     else
-      flash.now[:alert] = "Changes did not save"
+      flash.now[:alert] = "Changes did not save."
       render edit
     end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "New project created!"
+      redirect_to @project
+    else
+      flash.now[:alert] = "There was an error creating the project."
+      render new
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to root
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "projects#index"
   get "projects" => "projects#index"
-  get "projects/:id" => "projects#show", as: "project"
+  post "projects" => "projects#create"
+  get "projects/new" => "projects#new"
+  delete "projects/:id" => "projects#destroy"
   get "projects/:id/edit" => "projects#edit", as: "edit_project"
+  get "projects/:id" => "projects#show", as: "project"
+
   patch "projects/:id" => "projects#update"
+
+
 end
