71a24883e6fc7f571103159c1144fa9137995fac
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-28 09:52:19 -0500

    filter and modify index results

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = Project.where("deadline >= ?", Date.today)
   end
 
   def show
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,11 @@ module ProjectsHelper
     diff = deadline.to_date.mjd - Date.today.mjd
     return diff
   end
+
+  def goal_reached(project)
+    if project.funding >= project.goal
+      return true
+    end
+  end
+
 end
