08f5f68038e00097aad56d8749efb86f8a7f8bcd
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-28 00:21:46 -0500

    add edit route action and templates

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,25 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    if @project.save
+      flash[:notice] = "Project updated successfully"
+      redirect_to @project
+    else
+      flash.now[:alert] = "Changes did not save"
+      render edit
+    end
+  end
+
+private
+  def project_params
+    params.require(:project).permit(:title, :description, :funding, :goal, :deadline)
+  end
 end
